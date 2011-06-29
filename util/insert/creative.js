f8e4412b7b4cc69cfc998d819c22ff57d1062b00
Artem Semenov <xxxxxx@yandex-team.ru>
2011-06-29 17:29:10 +0400

    remove "localhost"

--- util/insert/creative.js
+++ util/insert/creative.js
@@ -5,10 +5,10 @@ db.creative.save({"id": 1, "flight_id": 1, "template_id": 8, "name": "creative#1
 }});
 
 db.creative.save({"id": 100, "flight_id": 100, "template_id": 7, "name": "creative#100", "uid": "", "state": "active", "params": {
-    "swf_url": 'http://localhost:8080/cstore/1/1/tks_beach2_240x400_kavanga.swf'
-  , "gif_url": 'http://localhost:8080/cstore/1/1/tks_beach2_240x400_kavanga.jpg'
+    "swf_url": 'http://adchange.co.cc/cstore/1/1/tks_beach2_240x400_kavanga.swf'
+  , "gif_url": 'http://adchange.co.cc/cstore/1/1/tks_beach2_240x400_kavanga.jpg'
 }});
 db.creative.save({"id": 101, "flight_id": 101, "template_id": 7, "name": "creative#101", "uid": "", "state": "active", "params": {
-    "swf_url": 'http://localhost:8080/cstore/1/1/240_400_Forex_Finam.swf'
-  , "gif_url": 'http://localhost:8080/cstore/1/1/240x400_Forex_Finam.gif'
+    "swf_url": 'http://adchange.co.cc/cstore/1/1/240_400_Forex_Finam.swf'
+  , "gif_url": 'http://adchange.co.cc/cstore/1/1/240x400_Forex_Finam.gif'
 }});
